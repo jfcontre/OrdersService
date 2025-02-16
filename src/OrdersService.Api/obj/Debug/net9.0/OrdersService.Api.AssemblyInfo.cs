12397e977f2aa72775f5f9d9de625c0e95889c9d
Jhon Contreras <jhon.contreras@ionix.cl>
2025-02-15 20:36:01 -0500

    Guarda cambios locales antes de rebase

--- src/OrdersService.Api/obj/Debug/net9.0/OrdersService.Api.AssemblyInfo.cs
+++ src/OrdersService.Api/obj/Debug/net9.0/OrdersService.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrdersService.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c580061377d44ea8c625fc19f59d89b22a7ff57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0f3d6dd893449de6ee4de1e844317ca8b33b8cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrdersService.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrdersService.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
